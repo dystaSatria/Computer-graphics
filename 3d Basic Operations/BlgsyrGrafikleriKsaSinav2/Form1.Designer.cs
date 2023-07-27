84294be838a48890a90f2401bf84d6e328a1079c
REZA DYSTA SATRIA <87264688+dystaSatria@users.noreply.github.com>
2023-07-27 11:51:03 +0300

    Add files via upload

---
+++ 3d Basic Operations/BlgsyrGrafikleriKsaSinav2/Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿
+namespace BlgsyrGrafikleriKsaSinav2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textXotele = new System.Windows.Forms.TextBox();
+            this.textYotele = new System.Windows.Forms.TextBox();
+            this.textZotele = new System.Windows.Forms.TextBox();
+            this.textZolcekle = new System.Windows.Forms.TextBox();
+            this.textYolcekle = new System.Windows.Forms.TextBox();
+            this.textXolcekle = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(13, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 55);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Eksenleri çiz(XYZ)";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(13, 95);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 61);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Cismin izometrik izdüşümünü al";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(13, 180);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(97, 90);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "ÖTELE";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.Yellow;
+            this.label1.Location = new System.Drawing.Point(116, 180);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "X";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.Yellow;
+            this.label2.Location = new System.Drawing.Point(117, 219);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Y";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.Yellow;
+            this.label3.Location = new System.Drawing.Point(116, 250);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(19, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Z";
+            // 
+            // textXotele
+            // 
+            this.textXotele.Location = new System.Drawing.Point(143, 180);
+            this.textXotele.Name = "textXotele";
+            this.textXotele.Size = new System.Drawing.Size(50, 20);
+            this.textXotele.TabIndex = 6;
+            // 
+            // textYotele
+            // 
+            this.textYotele.Location = new System.Drawing.Point(143, 216);
+            this.textYotele.Name = "textYotele";
+            this.textYotele.Size = new System.Drawing.Size(50, 20);
+            this.textYotele.TabIndex = 7;
+            // 
+            // textZotele
+            // 
+            this.textZotele.Location = new System.Drawing.Point(142, 250);
+            this.textZotele.Name = "textZotele";
+            this.textZotele.Size = new System.Drawing.Size(50, 20);
+            this.textZotele.TabIndex = 8;
+            // 
+            // textZolcekle
+            // 
+            this.textZolcekle.Location = new System.Drawing.Point(141, 374);
+            this.textZolcekle.Name = "textZolcekle";
+            this.textZolcekle.Size = new System.Drawing.Size(50, 20);
+            this.textZolcekle.TabIndex = 15;
+            // 
+            // textYolcekle
+            // 
+            this.textYolcekle.Location = new System.Drawing.Point(142, 340);
+            this.textYolcekle.Name = "textYolcekle";
+            this.textYolcekle.Size = new System.Drawing.Size(50, 20);
+            this.textYolcekle.TabIndex = 14;
+            // 
+            // textXolcekle
+            // 
+            this.textXolcekle.Location = new System.Drawing.Point(142, 304);
+            this.textXolcekle.Name = "textXolcekle";
+            this.textXolcekle.Size = new System.Drawing.Size(50, 20);
+            this.textXolcekle.TabIndex = 13;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.Color.Yellow;
+            this.label4.Location = new System.Drawing.Point(115, 374);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(19, 20);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Z";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.ForeColor = System.Drawing.Color.Yellow;
+            this.label5.Location = new System.Drawing.Point(116, 343);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(20, 20);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Y";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.ForeColor = System.Drawing.Color.Yellow;
+            this.label6.Location = new System.Drawing.Point(115, 304);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(20, 20);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "X";
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button4.Location = new System.Drawing.Point(13, 304);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(96, 90);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "ÖLÇEKLE";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button5.Location = new System.Drawing.Point(12, 559);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(96, 90);
+            this.button5.TabIndex = 16;
+            this.button5.Text = "TEMİZLE";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(884, 661);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textZolcekle);
+            this.Controls.Add(this.textYolcekle);
+            this.Controls.Add(this.textXolcekle);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textZotele);
+            this.Controls.Add(this.textYotele);
+            this.Controls.Add(this.textXotele);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textXotele;
+        private System.Windows.Forms.TextBox textYotele;
+        private System.Windows.Forms.TextBox textZotele;
+        private System.Windows.Forms.TextBox textZolcekle;
+        private System.Windows.Forms.TextBox textYolcekle;
+        private System.Windows.Forms.TextBox textXolcekle;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ 3d Basic Operations/BlgsyrGrafikleriKsaSinav2/Form1.cs
@@ -0,0 +1,322 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BlgsyrGrafikleriKsaSinav2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        System.Drawing.Graphics GrafikCiz;
+        Pen klm = new Pen(Color.Blue, 2);
+
+       double[,] Xekseni = new double[2, 4] { { 0, 0, 0, 1 }, { 2, 0, 0, 1 } };
+       double[,] Yekseni = new double[2, 4] { { 0, 0, 0, 1 }, { 0, 2, 0, 1 } };
+       double[,] Zekseni = new double[2, 4] { { 0, 0, 0, 1 }, { 0, 0, 2, 1 } };
+
+        double[,] Tizometrik = new double[4, 4]
+        {
+            {0.707,-0.408,0,0 },
+            {0,     0.816,0,0 },
+            {-0.707,-0.408,0,0 },
+            {0,0,0,0 }
+
+        };
+        double[,] XCisimNokta = new double[8, 4]
+        {
+            {0,0,0,1 },    //A
+            {1,0,0,1 },    //B
+            {1,0,1,1 },    //C
+            {0,0,1,1 },    //D
+            {0,1,0,1 },    //E
+            {1,1,0,1 },    //F
+            {1,1,1,1 },    //G
+            {0,1,1,1 }     //H
+        };
+        double[,] XKenar = new double[12, 2]
+        {
+            {0,1 },
+            {1,2 },
+            {2,3 },
+            {3,0 },
+            {4,5 },
+            {5,6 },
+            {6,7 },
+            {7,4 },
+            {0,4 },
+            {1,5 },
+            {2,6 },
+            {3,7 }
+        };
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //X'=X*Tiz
+            double[,] GeciciXekseni = new double[2, 4];
+            double[,] GeciciYekseni = new double[2, 4];
+            double[,] GeciciZekseni = new double[2, 4];
+
+            for(int i = 0; i < 2; i++)
+            {
+                for(int j = 0; j < 4; j++)
+                {
+                    GeciciXekseni[i, j] = 0;
+                    GeciciYekseni[i, j] = 0;
+                    GeciciZekseni[i, j] = 0;
+                    for(int k = 0; k < 4; k++)
+                    {
+                        GeciciXekseni[i, j] += Xekseni[i,k] * Tizometrik[k, j];
+                        GeciciYekseni[i, j] += Yekseni[i, k] * Tizometrik[k, j];
+                        GeciciZekseni[i, j] += Zekseni[i, k] * Tizometrik[k, j];
+                    }
+                }
+            }
+            klm.Color = Color.Black;
+            klm.Width = 4;
+            GrafikCiz.DrawLine(klm, KenarHesaplaX(GeciciXekseni[0, 0]), KenarHesaplaY(GeciciXekseni[0, 1]), KenarHesaplaX(GeciciXekseni[1, 0]), KenarHesaplaY(GeciciXekseni[1, 1]));
+            GrafikCiz.DrawLine(klm, KenarHesaplaX(GeciciYekseni[0, 0]), KenarHesaplaY(GeciciYekseni[0, 1]), KenarHesaplaX(GeciciYekseni[1, 0]), KenarHesaplaY(GeciciYekseni[1, 1]));
+            GrafikCiz.DrawLine(klm, KenarHesaplaX(GeciciZekseni[0, 0]), KenarHesaplaY(GeciciYekseni[0, 1]), KenarHesaplaX(GeciciZekseni[1, 0]), KenarHesaplaY(GeciciZekseni[1, 1]));
+
+        }
+        private int KenarHesaplaX(double geciciX)
+        {
+            return Convert.ToInt32(400 + (100 * geciciX));
+        }
+        private int KenarHesaplaY(double geciciY)
+        {
+            return Convert.ToInt32(250 + (-100 * geciciY));
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GrafikCiz = this.CreateGraphics();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //X'=X*Tiz
+            double[,] GeciciBKup = new double[8, 4];
+
+            for(int i = 0; i < 8; i++)
+            {
+                for(int j = 0; j < 4; j++)
+                {
+                    GeciciBKup[i, j] = 0;
+                    for(int k = 0; k < 4; k++)
+                    {
+                        GeciciBKup[i, j] += XCisimNokta[i, k] * Tizometrik[k, j];
+                    }
+                }
+            }
+            klm.Color = Color.BurlyWood;
+            klm.Width = 3;
+
+            GrafikCiz.DrawLine(klm, KenarHesaplaX(GeciciBKup[0, 0]), KenarHesaplaY(GeciciBKup[0, 1]), KenarHesaplaX(GeciciBKup[1, 0]), KenarHesaplaY(GeciciBKup[1, 1]));
+            GrafikCiz.DrawLine(klm, KenarHesaplaX(GeciciBKup[1, 0]), KenarHesaplaY(GeciciBKup[1, 1]), KenarHesaplaX(GeciciBKup[2, 0]), KenarHesaplaY(GeciciBKup[2, 1]));
+            GrafikCiz.DrawLine(klm, KenarHesaplaX(GeciciBKup[2, 0]), KenarHesaplaY(GeciciBKup[2, 1]), KenarHesaplaX(GeciciBKup[3, 0]), KenarHesaplaY(GeciciBKup[3, 1]));
+            GrafikCiz.DrawLine(klm, KenarHesaplaX(GeciciBKup[3, 0]), KenarHesaplaY(GeciciBKup[3, 1]), KenarHesaplaX(GeciciBKup[0, 0]), KenarHesaplaY(GeciciBKup[0, 1]));
+
+            GrafikCiz.DrawLine(klm, KenarHesaplaX(GeciciBKup[4, 0]), KenarHesaplaY(GeciciBKup[4, 1]), KenarHesaplaX(GeciciBKup[5, 0]), KenarHesaplaY(GeciciBKup[5, 1]));
+            GrafikCiz.DrawLine(klm, KenarHesaplaX(GeciciBKup[5, 0]), KenarHesaplaY(GeciciBKup[5, 1]), KenarHesaplaX(GeciciBKup[6, 0]), KenarHesaplaY(GeciciBKup[6, 1]));
+            GrafikCiz.DrawLine(klm, KenarHesaplaX(GeciciBKup[6, 0]), KenarHesaplaY(GeciciBKup[6, 1]), KenarHesaplaX(GeciciBKup[7, 0]), KenarHesaplaY(GeciciBKup[7, 1]));
+            GrafikCiz.DrawLine(klm, KenarHesaplaX(GeciciBKup[7, 0]), KenarHesaplaY(GeciciBKup[7, 1]), KenarHesaplaX(GeciciBKup[4, 0]), KenarHesaplaY(GeciciBKup[4, 1]));
+           
+            GrafikCiz.DrawLine(klm, KenarHesaplaX(GeciciBKup[0, 0]), KenarHesaplaY(GeciciBKup[0, 1]), KenarHesaplaX(GeciciBKup[4, 0]), KenarHesaplaY(GeciciBKup[4, 1]));
+            GrafikCiz.DrawLine(klm, KenarHesaplaX(GeciciBKup[1, 0]), KenarHesaplaY(GeciciBKup[1, 1]), KenarHesaplaX(GeciciBKup[5, 0]), KenarHesaplaY(GeciciBKup[5, 1]));
+            GrafikCiz.DrawLine(klm, KenarHesaplaX(GeciciBKup[2, 0]), KenarHesaplaY(GeciciBKup[2, 1]), KenarHesaplaX(GeciciBKup[6, 0]), KenarHesaplaY(GeciciBKup[6, 1]));
+            GrafikCiz.DrawLine(klm, KenarHesaplaX(GeciciBKup[3, 0]), KenarHesaplaY(GeciciBKup[3, 1]), KenarHesaplaX(GeciciBKup[7, 0]), KenarHesaplaY(GeciciBKup[7, 1]));
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double x, y, z;
+            if (textXotele.Text == "")
+            {
+                x = 0;
+            }
+            else
+            {
+                x = Convert.ToDouble(textXotele.Text);
+            }
+
+            if (textYotele.Text == "")
+            {
+                y = 0;
+            }
+            else
+            {
+                y = Convert.ToDouble(textYotele.Text);
+            }
+
+            if (textZotele.Text == "")
+            {
+                z = 0;
+            }
+            else
+            {
+                z = Convert.ToDouble(textZotele.Text);
+            }
+            double [,]Totele = new double[4, 4]
+            {
+        
+                { 1,0,0,0},
+                {0,1,0,0},
+                {0,0,1,0 },
+                {x,y,z,1 }
+            };
+            double[,] SonucOtele = new double[8, 4];
+
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    for (int k = 0; k < 4; k++)
+                    {
+                        SonucOtele[i, j] += XCisimNokta[i, k] * Totele[k,j];
+                    }
+                }
+            }
+
+            //X'=X*Tiz
+            double[,] GeciciBKup = new double[8, 4];
+
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    GeciciBKup[i, j] = 0;
+                    for (int k = 0; k < 4; k++)
+                    {
+                        GeciciBKup[i, j] +=SonucOtele[i, k] * Tizometrik[k, j];
+                    }
+                }
+            }
+
+            klm.Color = Color.Red;
+            klm.Width = 3;
+
+            GrafikCiz.DrawLine(klm, KenarHesaplaX(GeciciBKup[0, 0]), KenarHesaplaY(GeciciBKup[0, 1]), KenarHesaplaX(GeciciBKup[1, 0]), KenarHesaplaY(GeciciBKup[1, 1]));
+            GrafikCiz.DrawLine(klm, KenarHesaplaX(GeciciBKup[1, 0]), KenarHesaplaY(GeciciBKup[1, 1]), KenarHesaplaX(GeciciBKup[2, 0]), KenarHesaplaY(GeciciBKup[2, 1]));
+            GrafikCiz.DrawLine(klm, KenarHesaplaX(GeciciBKup[2, 0]), KenarHesaplaY(GeciciBKup[2, 1]), KenarHesaplaX(GeciciBKup[3, 0]), KenarHesaplaY(GeciciBKup[3, 1]));
+            GrafikCiz.DrawLine(klm, KenarHesaplaX(GeciciBKup[3, 0]), KenarHesaplaY(GeciciBKup[3, 1]), KenarHesaplaX(GeciciBKup[0, 0]), KenarHesaplaY(GeciciBKup[0, 1]));
+
+            GrafikCiz.DrawLine(klm, KenarHesaplaX(GeciciBKup[4, 0]), KenarHesaplaY(GeciciBKup[4, 1]), KenarHesaplaX(GeciciBKup[5, 0]), KenarHesaplaY(GeciciBKup[5, 1]));
+            GrafikCiz.DrawLine(klm, KenarHesaplaX(GeciciBKup[5, 0]), KenarHesaplaY(GeciciBKup[5, 1]), KenarHesaplaX(GeciciBKup[6, 0]), KenarHesaplaY(GeciciBKup[6, 1]));
+            GrafikCiz.DrawLine(klm, KenarHesaplaX(GeciciBKup[6, 0]), KenarHesaplaY(GeciciBKup[6, 1]), KenarHesaplaX(GeciciBKup[7, 0]), KenarHesaplaY(GeciciBKup[7, 1]));
+            GrafikCiz.DrawLine(klm, KenarHesaplaX(GeciciBKup[7, 0]), KenarHesaplaY(GeciciBKup[7, 1]), KenarHesaplaX(GeciciBKup[4, 0]), KenarHesaplaY(GeciciBKup[4, 1]));
+           
+            GrafikCiz.DrawLine(klm, KenarHesaplaX(GeciciBKup[0, 0]), KenarHesaplaY(GeciciBKup[0, 1]), KenarHesaplaX(GeciciBKup[4, 0]), KenarHesaplaY(GeciciBKup[4, 1]));
+            GrafikCiz.DrawLine(klm, KenarHesaplaX(GeciciBKup[1, 0]), KenarHesaplaY(GeciciBKup[1, 1]), KenarHesaplaX(GeciciBKup[5, 0]), KenarHesaplaY(GeciciBKup[5, 1]));
+            GrafikCiz.DrawLine(klm, KenarHesaplaX(GeciciBKup[2, 0]), KenarHesaplaY(GeciciBKup[2, 1]), KenarHesaplaX(GeciciBKup[6, 0]), KenarHesaplaY(GeciciBKup[6, 1]));
+            GrafikCiz.DrawLine(klm, KenarHesaplaX(GeciciBKup[3, 0]), KenarHesaplaY(GeciciBKup[3, 1]), KenarHesaplaX(GeciciBKup[7, 0]), KenarHesaplaY(GeciciBKup[7, 1]));
+           
+
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double x, y, z;
+            if (textXolcekle.Text == "")
+            {
+                x = 1;
+            }
+            else
+            {
+                x = Convert.ToDouble(textXolcekle.Text);
+            }
+            if (textYolcekle.Text == "")
+            {
+                y = 1;
+            }
+            else
+            {
+                y = Convert.ToDouble(textYolcekle.Text);
+            }
+            if (textZolcekle.Text == "")
+            {
+                z = 1;
+            }
+            else
+            {
+                z = Convert.ToDouble(textZolcekle.Text);
+            }
+            double[,] Tolcekle = new double[4, 4]
+            {
+
+                { x,0,0,0},
+                {0,y,0,0},
+                {0,0,z,0 },
+                {0,0,0,1 }
+            };
+
+            double[,] SonucOlcekle = new double[8, 4];
+
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    for (int k = 0; k < 4; k++)
+                    {
+                        SonucOlcekle[i, j] += XCisimNokta[i, k] * Tolcekle[k, j];
+                    }
+                }
+            }
+
+            //X'=X*Tiz
+            double[,] GeciciBKup = new double[8, 4];
+
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    GeciciBKup[i, j] = 0;
+                    for (int k = 0; k < 4; k++)
+                    {
+                        GeciciBKup[i, j] += SonucOlcekle[i, k] * Tizometrik[k, j];
+                    }
+                }
+            }
+
+            klm.Color = Color.Yellow;
+            klm.Width = 3;
+            GrafikCiz.DrawLine(klm, KenarHesaplaX(GeciciBKup[0, 0]), KenarHesaplaY(GeciciBKup[0, 1]), KenarHesaplaX(GeciciBKup[1, 0]), KenarHesaplaY(GeciciBKup[1, 1]));
+            GrafikCiz.DrawLine(klm, KenarHesaplaX(GeciciBKup[1, 0]), KenarHesaplaY(GeciciBKup[1, 1]), KenarHesaplaX(GeciciBKup[2, 0]), KenarHesaplaY(GeciciBKup[2, 1]));
+            GrafikCiz.DrawLine(klm, KenarHesaplaX(GeciciBKup[2, 0]), KenarHesaplaY(GeciciBKup[2, 1]), KenarHesaplaX(GeciciBKup[3, 0]), KenarHesaplaY(GeciciBKup[3, 1]));
+            GrafikCiz.DrawLine(klm, KenarHesaplaX(GeciciBKup[3, 0]), KenarHesaplaY(GeciciBKup[3, 1]), KenarHesaplaX(GeciciBKup[0, 0]), KenarHesaplaY(GeciciBKup[0, 1]));
+
+            GrafikCiz.DrawLine(klm, KenarHesaplaX(GeciciBKup[4, 0]), KenarHesaplaY(GeciciBKup[4, 1]), KenarHesaplaX(GeciciBKup[5, 0]), KenarHesaplaY(GeciciBKup[5, 1]));
+            GrafikCiz.DrawLine(klm, KenarHesaplaX(GeciciBKup[5, 0]), KenarHesaplaY(GeciciBKup[5, 1]), KenarHesaplaX(GeciciBKup[6, 0]), KenarHesaplaY(GeciciBKup[6, 1]));
+            GrafikCiz.DrawLine(klm, KenarHesaplaX(GeciciBKup[6, 0]), KenarHesaplaY(GeciciBKup[6, 1]), KenarHesaplaX(GeciciBKup[7, 0]), KenarHesaplaY(GeciciBKup[7, 1]));
+            GrafikCiz.DrawLine(klm, KenarHesaplaX(GeciciBKup[7, 0]), KenarHesaplaY(GeciciBKup[7, 1]), KenarHesaplaX(GeciciBKup[4, 0]), KenarHesaplaY(GeciciBKup[4, 1]));
+
+            GrafikCiz.DrawLine(klm, KenarHesaplaX(GeciciBKup[0, 0]), KenarHesaplaY(GeciciBKup[0, 1]), KenarHesaplaX(GeciciBKup[4, 0]), KenarHesaplaY(GeciciBKup[4, 1]));
+            GrafikCiz.DrawLine(klm, KenarHesaplaX(GeciciBKup[1, 0]), KenarHesaplaY(GeciciBKup[1, 1]), KenarHesaplaX(GeciciBKup[5, 0]), KenarHesaplaY(GeciciBKup[5, 1]));
+            GrafikCiz.DrawLine(klm, KenarHesaplaX(GeciciBKup[2, 0]), KenarHesaplaY(GeciciBKup[2, 1]), KenarHesaplaX(GeciciBKup[6, 0]), KenarHesaplaY(GeciciBKup[6, 1]));
+            GrafikCiz.DrawLine(klm, KenarHesaplaX(GeciciBKup[3, 0]), KenarHesaplaY(GeciciBKup[3, 1]), KenarHesaplaX(GeciciBKup[7, 0]), KenarHesaplaY(GeciciBKup[7, 1]));
+
+
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.Invalidate();
+        }
+    }
+}
